20374f368c770754ef831cbaa414a80aed391b44
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-03-17 20:54:55 +0000

    hw edit

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,15 @@
 'use strict';
 
+function getElementWidth(content, padding, border) {
 
-function getElementWidth(content, padding, border) { 
-    console.log(`${content + (padding * 2) + (border * 2)}`);
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding);
+    const borderWidth = parseFloat(border);
+
+    const elementWidth = contentWidth + (paddingWidth * 2) + (borderWidth * 2);
+
+    return elementWidth;
 }
-
-console.log(getElementWidth(Number.parseFloat("50px"), Number.parseFloat("8px"), Number.parseFloat("4px")));
-console.log(getElementWidth(Number.parseFloat("60px"), Number.parseFloat("12px"), Number.parseFloat("8.5px")));
-console.log(getElementWidth(Number.parseFloat("200px"), Number.parseFloat("0px"), Number.parseFloat("0px")));
+console.log(getElementWidth("50px", "8px", "4px")); 
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
